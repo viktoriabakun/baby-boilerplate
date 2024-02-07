eac349b4753738a7b373975028e3ab0ff1bc6da4
viktoriabakun <viktoriabakun00@gmail.com>
2024-02-07 17:30:40 +0100

    feat: add aliases for imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import { App } from '@app/index.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './app';
 import 'normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

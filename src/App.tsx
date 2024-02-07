f9c9e1d946b7a5621d15325ec269c7b695589523
viktoriabakun <viktoriabakun00@gmail.com>
2024-02-07 17:05:05 +0100

    chore: add aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import HomePage from './pages/home';
+import HomePage from '@pages/home';
 
 function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+import * as path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 });

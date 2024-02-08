7eceeedb3522c3770afb1f8e39aa4ed4891b13f9
viktoriabakun <viktoriabakun00@gmail.com>
2024-02-08 17:58:50 +0100

    chore: fix aliases

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { App } from '@app/index.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import 'normalize.css';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ import * as path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@app': `${path.resolve(__dirname, './src/app/')}`,
+      '@entities': `${path.resolve(__dirname, './src/entities/')}`,
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': `${path.resolve(__dirname, './src/pages')}`,
+      '@shared': `${path.resolve(__dirname, './src/shared')}`,
+      '@widgets': `${path.resolve(__dirname, './src/widgets')}`,
+    },
   },
 });

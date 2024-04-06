38b6df54f60bf72fd605678bc5634fb8a7ed041d
viktoriabakun <viktoriabakun00@gmail.com>
2024-04-06 16:43:38 +0200

    feat: add types to helpers

--- src/common/helpers/to-lower-case-compare.ts
+++ src/common/helpers/to-lower-case-compare.ts
@@ -1,3 +1,3 @@
-export const toLowerCaseCompare = (a: string, b: string) => {
-  return a && b && a.toLowerCase() === b.toLowerCase();
+export const toLowerCaseCompare = (a: string, b: string): boolean => {
+  return Boolean(a && b && a.toLowerCase() === b.toLowerCase());
 };

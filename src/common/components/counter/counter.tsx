810dcd85f12e759e392f55259554e7579a60b80c
viktoriabakun <viktoriabakun00@gmail.com>
2024-04-29 14:13:15 +0200

    fix: eslint errors

--- src/common/components/counter/counter.tsx
+++ src/common/components/counter/counter.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
-  const increment = () => setCount(prev => prev + 1);
+  const increment = () => setCount((prev) => prev + 1);
 
   return (
     <div className="flex items-center gap-5 text-[7vw]">

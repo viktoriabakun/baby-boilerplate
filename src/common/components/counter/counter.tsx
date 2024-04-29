142c4ebfb78050a3053fcda0fb0f2fd67348a8c8
viktoriabakun <viktoriabakun00@gmail.com>
2024-04-29 11:47:56 +0200

    chore: add tailwind, remove css files

--- src/common/components/counter/counter.tsx
+++ src/common/components/counter/counter.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
 
-import './styles.css';
-// @TODO: remove css file, add tailwind classes
-
 export const Counter = () => {
   const [count, setCount] = useState(0);
   const increment = () => setCount(prev => prev + 1);
 
   return (
-    <div className="counter">
+    <div className="flex items-center gap-5 text-[7vw]">
       <span>{count}</span>
       <button onClick={increment}>+</button>
     </div>

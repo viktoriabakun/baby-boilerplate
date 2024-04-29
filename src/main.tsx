c32595f75e56313fa9ec081a48bb197bfc3473b6
viktoriabakun <viktoriabakun00@gmail.com>
2024-04-29 12:03:30 +0200

    chore: remove normalize as there is Preflight in Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App.tsx';
 
 import './global.css';
-import 'normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

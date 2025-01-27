b2d0deb1b3527e74e9f86811fce4b3de85f9d605
viktoriabakun <viktoriabakun00@gmail.com>
2025-01-27 22:23:13 +0100

    chore: update deps, tailwind 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
